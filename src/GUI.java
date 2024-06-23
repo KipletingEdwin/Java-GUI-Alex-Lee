449f392c8d21f25035a3665f5f431728fada4d5b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-23 22:21:48 +0300

    add JFrame class

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class GUI {
+
+    public  GUI() {
+        JFrame frame = new JFrame();
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+
+    }
 }
