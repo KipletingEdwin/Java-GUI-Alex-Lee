e29c02060483d224c956d61be4aaf2edc5284985
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-23 23:08:12 +0300

    add border

--- src/GUI.java
+++ src/GUI.java
@@ -4,6 +4,11 @@ public class GUI {
 
     public  GUI() {
         JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+
+
     }
 
     public static void main(String[] args) {
